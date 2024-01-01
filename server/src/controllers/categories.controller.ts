77749008d8bea49de8c1e9d6295654d646bc0bdb
Kamil Hermann <kamilhermann@op.pl>
2024-01-01 10:46:24 +0100

    categories route

--- server/src/controllers/categories.controller.ts
+++ server/src/controllers/categories.controller.ts
@@ -1,13 +1,11 @@
-import express from "express";
+import { Request, Response } from "express";
 import getAllCategories from "../usecases/categories/get-all-categories.usecase";
 
-const categoriesController = express.Router();
 
-categoriesController.route('/')
-    .get((req, res) => {
+const getCategories = (req: Request, res: Response) => {
         getAllCategories().then(categories => {
             res.status(200).json(categories);
         });
-    });
+    };
 
-export default categoriesController;
+export { getCategories }
--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -1,24 +1,24 @@
 import express from 'express';
 import registerController from "../controllers/registration.controller";
 import recipesController from "../controllers/recipes.controller";
-import categoriesController from "../controllers/categories.controller";
 import dishesController from "../controllers/dishes.controller";
 import authRouter from "./auth.route";
 import favouritesController from "../controllers/favourites.controller";
 import tagsController from "../controllers/tags.controller";
 import usersController from "../controllers/users.controller";
 import activationRoute from "./activation.route";
+import categoriesRouter from "./categories.route";
 
 const router = express.Router();
 
+router.use('/activate', activationRoute);
+router.use('/auth', authRouter);
+router.use('/categories', categoriesRouter);
 router.use('/dishes', dishesController);
-router.use('/tags', tagsController);
-router.use('/categories', categoriesController);
+router.use('/favourites', favouritesController);
 router.use('/recipes', recipesController);
 router.use('/register', registerController);
-router.use('/auth', authRouter);
-router.use('/activate', activationRoute);
-router.use('/favourites', favouritesController);
+router.use('/tags', tagsController);
 router.use('/users', usersController);
 
 export { router };
